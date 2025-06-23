855b1351076f6fff54309341963d7cd8be3bf569
Laniakea <billiontrillionstars@gmail.com>
2025-06-22 20:52:36 -0400

    get first control pattern to compile!!

--- src/main/java/abilliontrillionstars/movesthemind/casting/actions/spells/OpMadeYouLook.java
+++ src/main/java/abilliontrillionstars/movesthemind/casting/actions/spells/OpMadeYouLook.java
@@ -1,40 +1,80 @@
 package abilliontrillionstars.movesthemind.casting.actions.spells;
 
+import abilliontrillionstars.movesthemind.Movesthemind;
+import at.petrak.hexcasting.api.casting.ParticleSpray;
+import at.petrak.hexcasting.api.casting.RenderedSpell;
 import at.petrak.hexcasting.api.casting.castables.Action;
-import at.petrak.hexcasting.api.casting.castables.ConstMediaAction;
+import at.petrak.hexcasting.api.casting.castables.SpellAction;
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment;
 import at.petrak.hexcasting.api.casting.eval.OperationResult;
 import at.petrak.hexcasting.api.casting.eval.vm.CastingImage;
 import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation;
 import at.petrak.hexcasting.api.casting.iota.Iota;
+import at.petrak.hexcasting.api.misc.MediaConstants;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class OpMadeYouLook implements ConstMediaAction
+import static org.spongepowered.asm.mixin.injection.selectors.ElementNode.listOf;
+
+public class OpMadeYouLook implements SpellAction
 {
     public static final Action INSTANCE = new OpMadeYouLook();
 
     @Override
-    public int getArgc() { return 1; }
-    @Override
-    public long getMediaCost() { return 0; }
+    public int getArgc() { return 2; }
 
     @Override
-    public @NotNull List<Iota> execute(@NotNull List<? extends Iota> list, @NotNull CastingEnvironment castingEnvironment)
+    public @NotNull Result executeWithUserdata(@NotNull List<? extends Iota> list, @NotNull CastingEnvironment castingEnvironment, @NotNull CompoundTag compoundTag)
     {
-        return List.of();
+        for (Iota i : list)
+            Movesthemind.LOGGER.info(""+i);
+
+        Entity target = null;
+        Vec3 dir = null;
+        return new Result(new Spell(target, dir),
+                MediaConstants.DUST_UNIT,
+                List.of(),
+                1);
     }
 
     @Override
-    public @NotNull CostMediaActionResult executeWithOpCount(@NotNull List<? extends Iota> list, @NotNull CastingEnvironment castingEnvironment)
+    public boolean hasCastingSound(@NotNull CastingEnvironment castingEnvironment) { return true; }
+    @Override
+    public boolean awardsCastingStat(@NotNull CastingEnvironment castingEnvironment) { return true; }
+
+
+    @Override
+    public @NotNull Result execute(@NotNull List<? extends Iota> list, @NotNull CastingEnvironment castingEnvironment)
     {
         return null;
     }
+
+    private class Spell implements RenderedSpell
+    {
+        public Spell(Entity target, Vec3 dir) {}
+
+        @Override
+        public void cast(@NotNull CastingEnvironment env)
+        {
+            Movesthemind.LOGGER.info("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        }
+
+        @Override
+        public @Nullable CastingImage cast(@NotNull CastingEnvironment castingEnvironment, @NotNull CastingImage castingImage)
+        {
+            return castingImage;
+        }
+    }
+
 
     @Override
     public @NotNull OperationResult operate(@NotNull CastingEnvironment castingEnvironment, @NotNull CastingImage castingImage, @NotNull SpellContinuation spellContinuation)
     {
-        return null;
+        return SpellAction.DefaultImpls.operate(this, castingEnvironment, castingImage, spellContinuation);
     }
 }
