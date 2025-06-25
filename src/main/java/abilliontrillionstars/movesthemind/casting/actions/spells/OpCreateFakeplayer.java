10e4c788931196126e912a1ac66e6984f83dd261
Laniakea <billiontrillionstars@gmail.com>
2025-06-24 20:58:53 -0400

    add mishaps to OpCreateFakeplayer & limit names to 16 chars

--- src/main/java/abilliontrillionstars/movesthemind/casting/actions/spells/OpCreateFakeplayer.java
+++ src/main/java/abilliontrillionstars/movesthemind/casting/actions/spells/OpCreateFakeplayer.java
@@ -14,6 +14,7 @@ import at.petrak.hexcasting.api.casting.iota.Iota;
 import at.petrak.hexcasting.api.casting.mishaps.MishapBadCaster;
 import at.petrak.hexcasting.api.casting.mishaps.MishapBadLocation;
 import at.petrak.hexcasting.api.misc.MediaConstants;
+import at.petrak.hexcasting.fabric.FabricHexConfig;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.MinecraftServer;
@@ -41,7 +42,22 @@ public class OpCreateFakeplayer implements SpellAction
         {
             JavaMishapThrower.throwMishap(new MishapBadLocation(pos, "too_far"));
         }
-        return new SpellAction.Result(new OpCreateFakeplayer.Spell(pos),
+        Entity caster = env.getCastingEntity();
+        if(!(caster instanceof ServerPlayer))
+            JavaMishapThrower.throwMishap(new MishapBadCaster());
+
+        String username = caster.getName().toString();
+        username = username.substring(username.indexOf('{')+1, username.length()-1);
+        while(username.length() < 16)
+            username = username.concat("_"); // pad the string to 16 characters
+
+        if(username.substring(12,16).equals("_bot"))
+            JavaMishapThrower.throwMishap(new MishapBadCaster()); // no grey-goo! bad fakeplayer! bad!
+
+        username = username.substring(12,16).concat("_bot");
+
+
+        return new SpellAction.Result(new OpCreateFakeplayer.Spell(pos, username),
                 MediaConstants.CRYSTAL_UNIT,
                 List.of(ParticleSpray.burst(pos, 1.0, 10)),
                 1);
@@ -60,18 +76,17 @@ public class OpCreateFakeplayer implements SpellAction
     private class Spell implements RenderedSpell
     {
         private Vec3 pos;
-        public Spell(Vec3 pos) {this.pos = pos;}
+        private String name;
+        public Spell(Vec3 pos, String name) {this.pos = pos; this.name = name;}
 
         @Override
         public void cast(@NotNull CastingEnvironment env)
         {
             MinecraftServer server = env.getWorld().getServer();
             CommandSourceStack sourceStack = server.createCommandSourceStack();
-            String compName = env.getCaster().getName().toString();
-            String username = compName.substring(compName.indexOf('{')+1, compName.length()-1);
-            // TODO: make this pad the name to 16 characters to distinguish from any real player
-            if(username.contains("_bot")) return; // no grey-goo! bad fakeplayer! bad!
-            server.getCommands().performPrefixedCommand(sourceStack, "player "+username+"_bot spawn at "+pos.x+" "+pos.y+" "+pos.z);
+
+            server.getCommands().performPrefixedCommand(sourceStack, "player "+name+" spawn at "+pos.x+" "+pos.y+" "+pos.z);
+            server.getCommands().performPrefixedCommand(sourceStack, "gamemode "+name+" survival");
         }
 
         @Override
